75178bdd6024fc4800459d750aa3772695760597
Igor Davydenko <iam@igordavydenko.com>
2017-05-14 21:56:31 +0300

    Fix to docs config. Seems like Read the Docs do not use Python 3.6 yet.

--- docs/conf.py
+++ docs/conf.py
@@ -27,7 +27,7 @@ from pathlib import Path
 rel = Path(__file__).parent.parent
 sys.path.insert(0, str(rel))
 
-with open(rel / 'aiohttp_middlewares' / '__init__.py') as handler:
+with open(str(rel / 'aiohttp_middlewares' / '__init__.py')) as handler:
     INIT_PY = handler.read()
 
 VERSION = re.findall("__version__ = '([^']+)'", INIT_PY)[0]
