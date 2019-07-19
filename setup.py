abbd6636f7fea044af3f61b9d04c46379713511f
Igor Davydenko <iam@igordavydenko.com>
2019-07-19 17:29:41 +0200

    chore: Drop Python 3.5 support in setup.py classifiers as well.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup_kwargs = {
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
-    'python_requires': '>=3.5.3,<4.0.0',
+    'python_requires': '>=3.6,<4.0',
 }
 
 
